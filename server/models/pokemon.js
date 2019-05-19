d888dd91cce55efee2a24c3aa3d3cd620b33c246
Alda Pontes <aldapontes@Aldas-MacBook-Pro.local>
2019-05-19 13:54:18 -0400

    Alda, adding tests

--- server/models/pokemon.js
+++ server/models/pokemon.js
@@ -1,53 +1,44 @@
 const { Model } = require('objection');
 const { transaction } = require('objection');
-// const db = require('..DB FILE ANDREW SENT)
+const db = require('./knexfile.ts')
 
 // pass the knex connection to objection
-// Model.knex(db)
+ Model.knex(db)
 
 class Pokemon extends Model {
   static get tableName() {
     return 'pokemon';
   }
-  getAll() {
-    return this.query().orderby('pokemonNumber');
+
+  getAll(txn) {
+    try {
+      const result = await transaction(knex, async (txn) => {
+        const pokemon = await Pokemon.query(txn).orderby('pokemonNumber');
+        return pokemon;
+        })
+      console.log("Yazza! You have caught 'em all!");
+    } catch (err) {
+      console.log('Womp, you have not all the pokemon in thine pocket');
+    }
   }
+
+  //get()
 }
 
 module.exports = Pokemon;
 
 /*
-getAll(txn: Transaction) ­ returns all Pokemon, ordered by pokemonNumber ascending
+DONE ● getAll(txn: Transaction) ­ returns all Pokemon, ordered by pokemonNumber ascending
+
+--> test: check that it returned something, test that it returns the table length?
+
 ● get(pokemonId: string, txn: Transaction) ­ returns a single Pokemon, and associated
 items
+
+
 ● create(input: IPokemonCreateInput, txn: Transaction) ­ creates and returns a Pokemon
 ● edit(pokemonId: string, pokemon: IPokemonEditInput, txn: Transaction) ­ edits an
 existing Pokemon
 ● delete(pokemonId: string, txn: Transaction) ­ marks a Pokemon as deleted, but does not
 actually delete it from the database
-
-
-class MinimalModel extends Model {
-  static get tableName() {
-    return 'someTableName';
-  }
-}
-
-
-
-
-
-
-try {
-  const returnValue = await transaction(Person.knex(), async (trx) => {
-    // Here you can use the transaction.
-
-    // Whatever you return from the transaction callback gets returned
-    // from the `transaction` function.
-    return 'the return value of the transaction';
-  });
-  // Here the transaction has been committed.
-} catch (err) {
-  console.log('pinneaple prices are too high! failed to fetch all your pokEs!')
-}
 */
